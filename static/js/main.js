5fcb76b3953a1cc3f16d65aa089465874bd8d439
savokiss <jaynaruto@qq.com>
2017-05-20 12:13:21 +0800

    fix: filter

--- static/js/main.js
+++ static/js/main.js
@@ -8,7 +8,12 @@ var locations = [
 function SiderViewModel() {
   var self = this;
   this.name = 'savo';
+  this.filter = '';
   self.locations = ko.observableArray(locations);
+
+  self.handleFilterChange = function() {
+    console.log(this.filter)
+  }
 }
 
 ko.applyBindings(new SiderViewModel());
