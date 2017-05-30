41821edb684a993cf022ba85aeff8a9392d69e7f
savokiss <jaynaruto@qq.com>
2017-05-30 19:08:33 +0800

    feat: directions

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,24 @@
-var locations = [
-  'savo',
-  'test',
-  'null',
-  'nothing'
+var locations = [{
+    name: '什刹海公园',
+    point: {
+      lat: 39.9204541,
+      lng: 116.3693653
+    }
+  },
+  {
+    name: '天安门广场',
+    point: {
+      lat: 39.9063748,
+      lng: 116.3925044
+    }
+  },
+  {
+    name: '奥体公园',
+    point: {
+      lat: 39.9942851,
+      lng: 116.3915031
+    }
+  }
 ]
 
 function SiderViewModel() {
@@ -12,15 +28,18 @@ function SiderViewModel() {
   this.siderVisible = ko.observable(true);
 
   // 动态绑定地址列表
-  self.filteredLots = ko.computed(function() {
-    return locations.filter(function(lot){
-      return lot.toLowerCase().indexOf(self.filter().toLowerCase()) > -1;
+  self.filteredLots = ko.computed(function () {
+    return locations.filter(function (lot) {
+      return lot.name.toLowerCase().indexOf(self.filter().toLowerCase()) > -1;
     });
   });
 
-  self.toggleSider = function() {
+  self.toggleSider = function () {
     self.siderVisible(!self.siderVisible());
-    // $('#sider').slideToggle();
+  }
+
+  self.markLot = function(lot){
+    console.log(lot)
   }
 }
 
